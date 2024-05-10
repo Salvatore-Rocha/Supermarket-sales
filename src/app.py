15361d515b1900c667e6072f6c8e5fcc10a6fa15
Salvatore Rocha <salvador.rocha.e@gmail.com>
2024-05-10 14:03:27 -0600

    Changes in file

--- src/app.py
+++ src/app.py
@@ -18,25 +18,84 @@ layout = go.Layout(template= pio.templates["flatly"])
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 
 # File
-market_sales = pd.read_csv(csv_file)
+#market_sales = pd.read_csv("https://github.com/Salvatore-Rocha/Supermarket-sales/blob/002314ff6501373a489db96a35c9bd205fdbff8b/supermarket_sales.csv")
 
-header = html.H4(
+header = html.H1(
     "This will be the header to this app", 
     className="bg-primary text-white p-2 mb-2 text-center"
                 )
 
-tab1 = dbc.Tab([dcc.Graph(id="line-chart", figure=px.line(template="bootstrap"))], label="Line Chart")
-tab2 = dbc.Tab([dcc.Graph(id="scatter-chart", figure=px.scatter(template="bootstrap"))], label="Scatter Chart")
-tab3 = dbc.Tab([grid], label="Grid", className="p-4")
-tabs = dbc.Card(dbc.Tabs([tab1, tab2, tab3]))
+def create_card_4modal(title,subtitle,P_text):
+    card = dbc.Card(
+        dbc.CardBody([
+                html.H3([html.I(className="bi bi-bank me-2"), title]),
+                html.H5("$8.3M"),
+                html.H6(html.I(subtitle, className="bi bi-caret-up-fill text-success")),
+                    ]),
+        className="text-center",
+        style={"width": "19rem"})   
+    return card 
 
+def create_tab(City):
+    tab = dbc.Tab([ 
+            html.H1(f"{City}"), 
+            dbc.Row([
+                dbc.Col([ 
+                    dbc.Row([
+                        create_card_4modal("Customers","Total customers of the period","C"),
+                        create_card_4modal("Sales","Total sales of the period","C"),
+                        create_card_4modal("Gross Income","Total gross income of the period","C")
+                            ]),
+                    html.Br(),
+                    dbc.Row([
+                        dbc.Col([
+                            html.H5("Select a month to see the sales behaviour"),
+                            dcc.RadioItems(['January', 'February','March'], 
+                                        'February', 
+                                        inline=True,
+                                        className="text-success",
+                                        inputStyle={"margin-left":"6px", "margin-right": "2px"})
+                            ],width=6 ),
+                        dbc.Col([
+                            html.H5("Select what you wanna see"),
+                            dcc.RadioItems(['Customers', 'Sales','Gross Income'], 
+                                       'Sales', 
+                                       inline=True,
+                                       className="text-success",
+                                       inputStyle={"margin-left":"6px", "margin-right": "2px"})
+                            ],width=6 ),
+                        dcc.Graph(id=f"barplot_for_{City}", figure= {} )
+                            ])
+                        ],width= 6),
+                dbc.Col([
+                        dcc.Graph(id=f"Pie_1_for_{City}", figure= {}),
+                        dcc.Graph(id=f"Pie_2_for_{City}", figure= {}),
+                        ],width=4),
+                dbc.Col([
+                        dcc.Graph(id=f"1Bar_plot_for{City}", figure= {}),
+                        ],width=2)
+                    ])       
+                ], label=f"{City} City",)
+    return tab
+
+#The branches are located ing Yangon, Naypyitaw, Mandalay
+tab1 = create_tab("Yangon")
+tab2 = create_tab("Naypyitaw")
+tab3 = create_tab("Mandalay")
+tab4 = create_tab("All")
+tabs = dbc.Card(dbc.Tabs([tab1, tab2, tab3, tab4], style={'font-style': 'italic'}))
 
 app =  dash.Dash(__name__, 
                  external_stylesheets= [dbc.themes.FLATLY, dbc.icons.FONT_AWESOME, dbc_css],)
-server = app.server
-app.layout = dbc.Container(
-    header
-)
+#server = app.server
+app.layout = dbc.Container([
+        header,
+        dbc.Row([ #Sli with 4 plots
+            tabs
+                ]),
+    
+],fluid=True,
+  className="dbc dbc-ag-grid")
 
 if __name__=='__main__':
     app.run_server(debug=True, port=8050)
